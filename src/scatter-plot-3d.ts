4090099fc1fa38e3e6d036abd3eb9dd11d759e28
Edwin Huang <edwinhuang@google.com>
2023-03-29 15:57:55 -0700

    fix container size issue

--- src/scatter-plot-3d.ts
+++ src/scatter-plot-3d.ts
@@ -2,12 +2,22 @@ import { ThreeScatterPlot } from "./three-chart/ThreeScatterPlot";
 
 export function drawViz(data: any) {
   // Container setup.
+  const html = document.querySelector("html");
+  if (html && html.style) {
+    html.style.height = "100%";
+  }
+  const body = document.querySelector("body");
+  if (body && body.style) {
+    body.style.height = "100%";
+  }
   let container = document.getElementById("container");
   if (container) {
     container.textContent = "";
   } else {
     container = document.createElement("div");
     container.id = "container";
+    container.style.width = "100%";
+    container.style.height = "100%";
     document.body.appendChild(container);
   }
 
