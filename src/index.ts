47a6515de2cc97c4cf55ed882fa75e2e65aeacf2
Edwin Huang <edwinhuang@google.com>
2023-11-15 12:31:45 -0800

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { drawViz } from "./scatter-plot-3d";
-import { subscribeToData, objectTransform } from "@google/dscc";
 import { demoData } from "./three-chart/demo_data";
 
-// Subscribe to data and style changes. Use the table format for data.
+declare global {
+  interface Window {
+    google: {
+      lookerstudio: any;
+    };
+  }
+}
+
+const devLib = window.google.lookerstudio;
+
 // TODO remove mock data
-subscribeToData(() => drawViz(demoData.youtubeSubscriber), {
-  transform: objectTransform,
-});
+devLib.registerVisualization((data: any) => {
+  drawViz(demoData.youtubeSubscriber);
+  console.log(data);
+}, devLib.tableTransform);
--- src/scatter-plot-3d.ts
+++ src/scatter-plot-3d.ts
@@ -2,24 +2,15 @@ import { ThreeScatterPlot } from "./three-chart/ThreeScatterPlot";
 
 export function drawViz(data: any) {
   // Container setup.
-  const html = document.querySelector("html");
-  if (html && html.style) {
-    html.style.height = "100%";
-  }
-  const body = document.querySelector("body");
-  if (body && body.style) {
-    body.style.height = "100%";
-  }
   let container = document.getElementById("container");
   if (container) {
     container.textContent = "";
   } else {
     container = document.createElement("div");
     container.id = "container";
-    container.style.width = "100%";
-    container.style.height = "100%";
     document.body.appendChild(container);
   }
+  container.style.height = "100%";
 
   // Render the viz.
   new ThreeScatterPlot({
